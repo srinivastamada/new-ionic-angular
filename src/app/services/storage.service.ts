a03e56cfe29772aac900f052c08ab8f3228e438d
Srinivas Tamada <srinivas@9lessons.info>
2019-08-11 23:31:10 -0400

    Storage

---
+++ src/app/services/storage.service.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@angular/core';
+import { Plugins } from '@capacitor/core';
+
+const { Storage } = Plugins;
+@Injectable({
+  providedIn: 'root'
+})
+export class StorageService {
+
+  constructor() { }
+
+  // JSON "set" example
+async setObject() {
+  await Storage.set({
+    key: 'user',
+    value: JSON.stringify({
+      id: 1,
+      name: 'Max'
+    })
+  });
+}
+
+// JSON "get" example
+async getObject() {
+  const ret = await Storage.get({ key: 'user' });
+  const user = JSON.parse(ret.value);
+}
+
+async setItem() {
+  await Storage.set({
+    key: 'name',
+    value: 'Max'
+  });
+}
+
+async getItem() {
+  const value = await Storage.get({ key: 'name' });
+  console.log('Got item: ', value);
+}
+
+async removeItem() {
+  await Storage.remove({ key: 'name' });
+}
+
+async keys() {
+  const keys = await Storage.keys();
+  console.log('Got keys: ', keys);
+}
+
+async clear() {
+  await Storage.clear();
+}
+}
