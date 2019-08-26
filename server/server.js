ee7cc1333f9718cc1555d9bd95ab068c3492102f
Srinivas Tamada <srinivas@9lessons.info>
2019-08-26 15:42:04 +0000

    server

--- server/server.js
+++ server/server.js
@@ -3,7 +3,11 @@ let app = express();
 
 let bodyParser = require('body-parser');
 let backendPort = 8084;
-let localhostPort = process.env.PORT || 8100;
+
+let mock = {
+    'userData': require('./mock/userData'),
+    'feedData': require('./mock/feedData')
+}
 
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
@@ -24,6 +28,26 @@ app.use(
   })
 );
 
+app.post('/login', function(req, res, next){
+    let data = JSON.parse(req.body);
+    let username = data.username;
+    let password = data.password;
+
+    if(username === 'srinivas' && password === '9lessons'){
+      return res.status(200).json(mock.userData);
+    } else {
+       return res.status(200).send('{"error":{"text":"Bad request wrong username and password"}}');
+    }
+});
+
+app.post('/feed', function(req, res, next) {
+  let data = JSON.parse(req.body);
+  if (data.token && data.user_id === '1') {
+      return res.status(200).json(mock.feedData);
+  } else {
+    return res.status(401).send('No Access');
+  }
+});
 
 
 app.listen(backendPort, function() {
