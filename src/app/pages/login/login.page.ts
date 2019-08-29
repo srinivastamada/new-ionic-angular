6f294d8ccb428fa9dfec15a2d049e711f9961f81
Srinivas Tamada <srinivas@9lessons.info>
2019-08-29 19:42:48 +0000

    Revert "change"
    This reverts commit 26098170c61e22c08151cd63ed0ff969df092774.

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { AuthService } from '../../services/auth.service';
-import { StorageService } from '../../services/storage.service';
-import { AuthConstants } from '../../config/auth-constants';
 
 @Component({
   selector: 'app-login',
@@ -10,45 +7,11 @@ import { AuthConstants } from '../../config/auth-constants';
   styleUrls: ['./login.page.scss']
 })
 export class LoginPage implements OnInit {
-   postData = {
-       username:'',
-       password:''
-   };
-   error = false;
-   errorMessage:string = '';
-
-  constructor(private router: Router, private authService: AuthService, private storageService: StorageService) {}
+  constructor(private router: Router) {}
 
   ngOnInit() {}
 
-   validateInputs(){
-    let username = this.postData.username.trim();
-    let password = this.postData.password.trim();
-    return this.postData.username && this.postData.password && username.length > 0 && password.length > 0;
-  }
-
-  loginAction() {
-     if (this.validateInputs()) {
-    console.log(this.postData);
-      this.authService.login(this.postData).subscribe(
-        (res: any) => {
-          if (res.userData) {
-            this.error = false;
-            this.storageService.store(AuthConstants.AUTH, res.userData);
-            this.router.navigate(['home/feed']);
-          } else {
-            this.error = true;
-            this.errorMessage = "The email/username or password you entered is incorrect. You can try again or get some help logging in."
-          }
-        },
-        (error: any) => {
-          this.error = true;
-          this.errorMessage = "The email/username or password you entered is incorrect. You can try again or get some help logging in"
-        }
-      );
-    } else {
-      this.error = true;
-      this.errorMessage = "Please enter email/username or password."
-    }
+  login() {
+    this.router.navigate(['/home/feed']);
   }
 }
